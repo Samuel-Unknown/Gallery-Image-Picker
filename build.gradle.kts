17be38ce840584155cf5c595724a17b278fac58d
Samuel-Unknown <samuelunknown@gmail.com>
2021-10-11 11:29:50 +0300

    prepare to publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-import java.util.Properties
-import java.io.FileReader
-
-plugins {
-    id(MAVEN_PUBLISH_PLUGIN)
-}
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -29,61 +22,6 @@ allprojects {
     project.tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_11.toString()
-        }
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                artifact("$buildDir/outputs/aar/library-release.aar")
-
-                groupId = Publishing.groupId
-                artifactId = Publishing.artifactId
-                version = Publishing.version
-
-                pom {
-                    name.set(Publishing.POM.name)
-                    description.set(Publishing.POM.description)
-                    url.set(Publishing.POM.url)
-
-                    licenses {
-                        license {
-                            name.set(Publishing.License.name)
-                            url.set(Publishing.License.url)
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set(Publishing.Developer.id)
-                            name.set(Publishing.Developer.name)
-                            email.set(Publishing.Developer.email)
-                        }
-                    }
-
-                    scm {
-                        url.set(Publishing.POM.url)
-                    }
-                }
-            }
-        }
-
-        repositories {
-            maven {
-                name = Publishing.Repository.Properties.File.name
-                url = uri(Publishing.Repository.url)
-
-                credentials {
-                    val propertiesFile = rootProject.file(Publishing.Repository.Properties.File.name)
-                    val properties = Properties()
-                    FileReader(propertiesFile).use { reader -> properties.load(reader) }
-
-                    username = properties.getProperty(Publishing.Repository.Properties.userId)
-                    password = properties.getProperty(Publishing.Repository.Properties.token)
-                }
-            }
         }
     }
 }
--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -1,7 +1,7 @@
 @file:Suppress("MayBeConstant")
 
 object Publishing {
-    val groupId = "com.samuelunknown"
+    val groupId = "io.github.samuel-unknown"
     val artifactId = "gallery-image-picker"
     val version = Versions.Library.versionName
 
@@ -9,31 +9,47 @@ object Publishing {
         val url = "https://github.com/samuel-unknown/gallery-image-picker"
         val name = "Gallery Image Picker"
         val description = "Android library for picking images"
+
+        object License {
+            val url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            val name = "The Apache License, Version 2.0"
+        }
+
+        object Developer {
+            val id = "samuel-unknown"
+            val name = "Samuel Unknown"
+            val email = "samuelunknown@gmail.com"
+        }
     }
 
-    object License {
-        val url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-        val name = "The Apache License, Version 2.0"
-    }
+    object Properties {
+        val FileName = "publish.properties"
 
-    object Developer {
-        val id = "samuel-unknown"
-        val name = "Samuel Unknown"
-        val email = "samuelunknown@gmail.com"
+        object GitHub {
+            val userId = "GITHUB_USER_ID"
+            val token = "GITHUB_TOKEN"
+        }
+
+        object Sonatype {
+            val username = "SONATYPE_USERNAME"
+            val password = "SONATYPE_PASSWORD"
+        }
+
+        object Signing {
+            val KeyId = "PGP_SIGNING_KEY_ID"
+            val Key = "PGP_SIGNING_KEY"
+            val Password = "PGP_SIGNING_PASSWORD"
+        }
     }
 
     object Repository {
-        val name = "githubPackages"
-        val url = "https://maven.pkg.github.com/samuel-unknown/gallery-image-picker"
-
-        object Properties {
-            val userId = "USER_ID"
-            val token = "TOKEN"
-
-            object File {
-                val name = "github.properties"
-                val token = "TOKEN"
-            }
+        object GitHub {
+            val name = "GitHubPackages"
+            val url = "https://maven.pkg.github.com/samuel-unknown/gallery-image-picker"
+        }
+        object SonatypeStage {
+            val name = "SonatypeStaging"
+            val url = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
         }
     }
 }
