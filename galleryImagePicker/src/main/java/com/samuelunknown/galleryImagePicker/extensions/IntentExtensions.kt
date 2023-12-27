c875b15be3cf5173f041d7f7ab2c5c523bcd94a4
Samuel Unknown <samuelunknown@gmail.com>
2023-12-27 17:54:59 +0700

    popup menu customisation

--- galleryImagePicker/src/main/java/com/samuelunknown/galleryImagePicker/extensions/IntentExtensions.kt
+++ galleryImagePicker/src/main/java/com/samuelunknown/galleryImagePicker/extensions/IntentExtensions.kt
@@ -1,6 +1,7 @@
 package com.samuelunknown.galleryImagePicker.extensions
 
 import android.content.Intent
+import android.os.Build
 import com.samuelunknown.galleryImagePicker.domain.model.GalleryConfigurationDto
 import com.samuelunknown.galleryImagePicker.domain.model.ImagesResultDto
 import java.lang.Exception
@@ -9,7 +10,7 @@ private const val EXTRA_IMAGES_RESULT_DTO = "EXTRA_IMAGES_RESULT_DTO"
 private const val EXTRA_CONFIGURATION_DTO = "EXTRA_CONFIGURATION_DTO"
 
 internal fun Intent.getImagesResultDto(): ImagesResultDto? {
-    return this.getParcelableExtra(EXTRA_IMAGES_RESULT_DTO)
+    return this.getParcelableItem(EXTRA_IMAGES_RESULT_DTO)
 }
 
 internal fun Intent.putImagesResultDto(activityResultDto: ImagesResultDto): Intent {
@@ -17,10 +18,19 @@ internal fun Intent.putImagesResultDto(activityResultDto: ImagesResultDto): Inte
 }
 
 internal fun Intent.getGalleryConfigurationDto(): GalleryConfigurationDto {
-    return this.getParcelableExtra(EXTRA_CONFIGURATION_DTO)
+    return this.getParcelableItem(EXTRA_CONFIGURATION_DTO)
         ?: throw Exception("${GalleryConfigurationDto::class.java} not found.")
 }
 
 internal fun Intent.putGalleryConfigurationDto(activityResultDto: GalleryConfigurationDto): Intent {
     return this.putExtra(EXTRA_CONFIGURATION_DTO, activityResultDto)
+}
+
+internal inline fun <reified T> Intent.getParcelableItem(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableExtra(key)
+    }
 }
